8802dda0dd1381576d8a0c7d2a36abdb00c1d0c1
jonathanmartineztek <jonathanmartineztek@gmail.com>
2018-10-31 04:23:16 -0700

    Update main.py
    - added sunset functionality

--- main.py
+++ main.py
@@ -3,19 +3,50 @@
 import ephem #pip install pyephem
 import datetime
 import os
+from pytz import timezone
 import urllib2
 
+currentDate = datetime.datetime.now(timezone("US/Pacific"))
+nextDate = currentDate + datetime.timedelta(days=1)
 spottedAfter = 1
 
+def display():
+	global home
+	
+	home = ephem.Observer()
+	home.lat = "33.98"
+	home.lon = "-117.37"
+	home.elevation = 260
+	#To get U.S. Naval Astronomical Almanac values, use these settings
+	home.pressure = 0
+	home.horizon = "-0:34"
+
+	readLog()
+	if onLine():
+		if len(logB) == 3:
+			print(chodesh + " " + str(yom))
+		elif len(logB) == 2:
+			getNewMoon()
+			print(chodesh + " " + str(yom))
+	elif not onLine():
+		if len(logB) == 3:
+			print(chodesh + " " + str(yom) + "*")
+		else:
+			print("offLine")
+
 def onLine():
-    try:
-        urllib2.urlopen('http://www.google.com', timeout=1)
-        return True
-    except urllib2.URLError as err:
-        return False
+	try:
+		urllib2.urlopen('http://www.google.com', timeout=1)
+		return True
+	except urllib2.URLError as err:
+		return False
 
 def readLog():
-	inFile = open("log.txt")
+	global log
+	
+	log = os.path.expanduser('~') + "/Documents/t/log.txt" 
+	#'/Users/mrsir' + 'Documents/t/log/txt'
+	inFile = open(log)
 	try:
 		global chodesh
 		global yyyymm
@@ -26,6 +57,7 @@ def readLog():
 
 		content = inFile.readlines()
 		log = content[len(content)-1].split(";")
+
 		chodesh = getChodesh(log[0])
 		yyyymm = log[1]
 
@@ -36,13 +68,13 @@ def readLog():
 			day = int(logB[2])
 			roshChodesh = datetime.date(year, month, day+spottedAfter)
 			getDaysSince(roshChodesh)
+
 	finally:
 		inFile.close()
 
 def updateLog(update):
-	#input <type 'str'>
 	if len(logB) == 2:
-		inFile = open("log.txt", "a")
+		inFile = open(log, "a")
 		try:
 			inFile.write(update)
 		finally:
@@ -61,13 +93,14 @@ def getChodesh(num):
 		raise ValueError('Not a month')
 
 def getDaysSince(roshChodesh):
-	#input <type 'datetime'>
 	global yom
-	yom = (datetime.date.today() - roshChodesh).days
+	
+	to_day = datetime.date(currentDate.year,currentDate.month, currentDate.day)
+	yom = (to_day - roshChodesh).days + isNowInTimePeriod()
 
 def getNewMoon():
 	global yyyymm
-	#input <type 'str'>
+	
 	n = str(ephem.next_new_moon(yyyymm))
 	# 2018/10/9 03:46:51
 	n = n.split(" ")
@@ -78,22 +111,40 @@ def getNewMoon():
 	readLog()
 
 	#return <type 'datetime.date'>
-	return datetime.date(int(n[0]), int(n[1]), int(n[2])+spottedAfter)
+	return datetime.date(int(n[0]), int(n[1]), int(n[2]) + spottedAfter)
 
+def getIllumination():
+	moon = ephem.Moon()
+	moon.compute(home)
 
-readLog()
-if onLine():
-	if len(logB) == 3:
-		print(chodesh + " " + str(yom))
-	elif len(logB) == 2:
-		getNewMoon()
-		print(chodesh + " " + str(yom))
-elif not onLine():
-	if len(logB) == 3:
-		print(chodesh + " " + str(yom) + "*")
-	else:
-		print("offLine")
+	return float(moon.moon_phase * 100)
 
+def getSunseTime():
+	global sunseTime
+	
+	sun = ephem.Sun()
+	sun.compute(nextDate.strftime("%Y/%m/%d"))
+	nowSet = home.next_setting(sun).datetime()
+	nowSetUTC = naive2aware(nowSet) #<type 'datetime.datetime'>
+	nowSetPacific = UTC2Pacific(nowSetUTC)
+	sunseTime = nowSetPacific.strftime("%X")
 
-#Python 2.7: https://repl.it/repls/ProudImmediateUser
-#Python 3: https://repl.it/repls/RealFatLicensing
+	#return <type 'str'>
+	return nowSetPacific
+
+def naive2aware(date):
+	return timezone("UTC").localize(date)
+
+def UTC2Pacific(time):
+	return time.astimezone(timezone('US/Pacific'))
+
+def isNowInTimePeriod():
+	nextSet = getSunseTime()
+	toDate = currentDate.replace(currentDate.year,currentDate.month,currentDate.day, 23, 59, 59)
+	
+	return 1 if nextSet < currentDate and currentDate <= toDate else 0
+
+display()
+print("---")
+print("Next Sunset(" + getSunseTime().strftime("%x): %X"))
+print("Moon Illumination: " + str(round(getIllumination(), 1)) + "%")
