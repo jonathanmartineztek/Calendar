bfdabc893141c770ba9bb962c5245731a717176c
jonathanmartineztek <jonathanmartineztek@gmail.com>
2018-11-01 14:23:59 -0700

    Update main.py
    - offline functionality
    - displays next current sunset
    - observes next chodesh

--- main.py
+++ main.py
@@ -1,42 +1,34 @@
 #!/usr/bin/env python
 
+#https://repl.it/@jonathanmartinez/FussyHuskyProgram
+
 import ephem #pip install pyephem
 import datetime
 import os
 from pytz import timezone
 import urllib2
 
+nowPacific = datetime.datetime.now(timezone("US/Pacific"))
 nowUTC = datetime.datetime.utcnow().strftime("%Y/%m/%d")
 nextUTC = (datetime.datetime.utcnow() + datetime.timedelta(days=1)).strftime("%Y/%m/%d")
-nowPacific = datetime.datetime.now(timezone("US/Pacific"))
 spottedAfter = 1
 
-def display():
-	readLog()
-	if onLine():
-		if len(logB) == 3:
-			print(chodesh + " " + str(yom))
-		elif len(logB) == 2:
-			getNewMoon()
-			print(chodesh + " " + str(yom))
-	elif not onLine():
-		if len(logB) == 3:
-			print(chodesh + " " + str(yom) + "*")
-		else:
-			print("offLine")
+def obServer():
+	home = ephem.Observer()
+	home.lat = "33.98"
+	home.lon = "-117.37"
+	home.elevation = 260
+	#To get U.S. Naval Astronomical Almanac values, use these settings
+	home.pressure = 0
+	home.horizon = "-0:34"
 
-def onLine():
-	try:
-		urllib2.urlopen('http://www.google.com', timeout=1)
-		return True
-	except urllib2.URLError as err:
-		return False
+	return home
 
 def readLog():
 	global log
-	
-	log = os.path.expanduser('~') + "/Documents/t/log.txt"
-	inFile = open("log.txt")
+
+	log = os.path.expanduser('~') + "/Documents/log.txt"
+	inFile = open(log)
 	try:
 		global chodesh
 		global yyyymm
@@ -44,6 +36,8 @@ def readLog():
 		global month
 		global day
 		global logB
+		global yom
+		global disPlay
 
 		content = inFile.readlines()
 		log = content[len(content)-1].split(";")
@@ -54,17 +48,25 @@ def readLog():
 		logB = log[1].split("/")
 		year = int(logB[0])
 		month = int(logB[1])
-		if len(logB) == 3:
+		if len(logB) == 2:
+			getNewMoon()
+		elif len(logB) == 3:
+			disPlay = True
 			day = int(logB[2])
 			roshChodesh = datetime.date(year, month, day+spottedAfter)
-			getDaysSince(roshChodesh)
+			daySince(roshChodesh)
+			if isNowInTimePeriod():
+				if yom == 29 or yom == 30:
+					if getIllumination() >= 1.5:
+						updateLog("\n" + str(int(log[0])+1) + ";" + str(nowUTC.year) + "/" + str(nowUTC.month))
+						getNewMoon()
 
 	finally:
 		inFile.close()
 
 def updateLog(update):
-	if len(logB) == 2:
-		inFile = open("log.txt", "a")
+	if len(logB) == 2 or "\n" in update:
+		inFile = open(log, "a")
 		try:
 			inFile.write(update)
 		finally:
@@ -82,18 +84,16 @@ def getChodesh(num):
 	except:
 		raise ValueError('Not a month')
 
-def getDaysSince(roshChodesh):
+def daySince(roshChodesh):
 	global yom
-	
+
 	to_day = datetime.date(nowPacific.year,nowPacific.month, nowPacific.day)
 	yom = (to_day - roshChodesh).days + isNowInTimePeriod()
 
 def getNewMoon():
 	global yyyymm
-	
-	n = str(ephem.next_new_moon(yyyymm))
-	# 2018/10/9 03:46:51
-	n = n.split(" ")
+
+	n = str(ephem.next_new_moon(yyyymm)).split(" ")
 	# ["2018/10/9", "03:46:51"]
 	n = n[0].split("/")
 	#["2018", "10", "9"]
@@ -103,17 +103,6 @@ def getNewMoon():
 	#return <type 'datetime.date'>
 	return datetime.date(int(n[0]), int(n[1]), int(n[2]) + spottedAfter)
 
-def obServer():
-	home = ephem.Observer()
-	home.lat = "33.98"
-	home.lon = "-117.37"
-	home.elevation = 260
-	#To get U.S. Naval Astronomical Almanac values, use these settings
-	home.pressure = 0
-	home.horizon = "-0:34"
-	
-	return home
-
 def getIllumination():
 	moon = ephem.Moon()
 	moon.compute(obServer())
@@ -122,7 +111,7 @@ def getIllumination():
 
 def getSunseTime(date):
 	global sunseTime
-	
+
 	home = obServer()
 	home.date = date
 	sun = ephem.Sun()
@@ -131,7 +120,7 @@ def getSunseTime(date):
 	nowSetUTC = naive2aware(nowSet) #<type 'datetime.datetime'>
 	nowSetPacific = UTC2Pacific(nowSetUTC)
 	sunseTime = nowSetPacific.strftime("%X")
-	
+
 	#return <type 'str'>
 	return nowSetPacific
 
@@ -147,7 +136,24 @@ def isNowInTimePeriod():
 
 	return 1 if nowSet < nowPacific and nowPacific <= toDate else 0
 
+def onLine():
+	try:
+		urllib2.urlopen('http://www.google.com', timeout=1)
+		return True
+	except urllib2.URLError as err:
+		return False
+
+def display():
+	readLog()
+	if onLine() and disPlay:
+			print(chodesh + " " + str(yom))
+	elif not onLine() and disPlay:
+			print(chodesh + " " + str(yom) + "*")
+
 display()
 print("---")
-print("Next Sunset(" + getSunseTime(nextUTC).strftime("%x): %X"))
 print("Moon Illumination: " + str(getIllumination()) + "%")
+if getSunseTime(nowUTC) <= nowPacific:
+	 print("Next Sunset(" + getSunseTime(nextUTC).strftime("%x): %X"))
+else:
+	print("Next Sunset(" + getSunseTime(nowUTC).strftime("%x): %X"))
